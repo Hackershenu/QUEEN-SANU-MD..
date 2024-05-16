8d4200395d980dc3981c8121ab3ac218b88081e2
Hackershenu <hackershenuwa@gmail.com>
2024-05-16 12:28:36 +0530

    Update Start.js

--- Start.js
+++ Start.js
@@ -1,2 +1,3 @@
 * @project_name DARK-MD
-* @authors Dark Shenuwa & Alfz-(Sadaru)
+* @authors Dark Shenuwa & Alfa
+* @description The main hope of creating this bot is to take full advantage of the WhatsApp app and make its work easier
